ee2d4c5af61ac477173bcee32f3ace42563e2dbc
Adri-rodri <adrian08rmicloud.com@gmail.com>
2026-05-26 09:16:52 -0600

    Se cambio el fondo, se agrego sprite sheets de prueba para player, y fix de resize-> se agrego una camara para que todo se mantenga del mismo size

--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
@@ -1,6 +1,8 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -9,41 +11,65 @@ import com.badlogic.gdx.utils.ScreenUtils;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    //private Texture image;
+    private Texture image,playerSheet;
     ShapeRenderer shape;
     private Player player;
+    OrthographicCamera camera;
+    
 
     @Override
     public void create() {
+        camera= new OrthographicCamera();
+        camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         batch = new SpriteBatch();
-        //image = new Texture("libgdx.png");
+        image = new Texture("fondo.png");
+        playerSheet=new Texture("playerSheet.png");
         shape=new ShapeRenderer();
         player= new Player(0,0);
+        player.cargarSprites(playerSheet, playerSheet, playerSheet);
+        player.setCabeza(0, 0);
         
         
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        //batch.draw(image, 140, 210);
-        batch.end();
-        player.tecladoInput();
-        shape.begin(ShapeRenderer.ShapeType.Filled);
-        player.dibujarPlayer(shape);
-        shape.end();
+        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1f); //resetea la pantalla a negro cda frame
+        
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        shape.setProjectionMatrix(camera.combined);
+        
+        batch.begin();//Trabajar texturas entre aqui
+        dibujarTexturas(batch);
+        batch.end();//y aqui
+        //Logica
+        logic();
+        //Player temporal con figura
+//        shape.begin(ShapeRenderer.ShapeType.Filled);
+//        player.dibujarPlayer(shape);
+//        shape.end();
 
     }
     @Override
     public void resize(int width, int height) {
-        batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+        camera.viewportWidth=width;
+        camera.viewportHeight=height;
+        camera.update();
+    }
+    public void dibujarTexturas(SpriteBatch batch){ //dibujar todas las texturas aqui
+        batch.draw(image, 0, 0,832,640);
+        player.dibujarPlayer(batch);
+    }
+    public void logic(){//aqui se configura toda la logicaa del juego
+        player.tecladoInput();
     }
 
     @Override
-    public void dispose() {
+    public void dispose(){
         batch.dispose();
-        //image.dispose();
+        image.dispose();
+        shape.dispose();
     }
     
 }
--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Player.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Player.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /**
@@ -11,28 +13,40 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
  * @author adria
  */
 public class Player extends Entity{
-    private Texture sheetCabeza;
-    private Texture sheetTorso;
-    private Texture sheetAccesorio;
+    private Texture sheetCabeza,sheetTorso,sheetAccesorio;
+    
+    protected TextureRegion playerSprite;
     public Player(int x, int y) {
         super(x, y);
     }
     public void tecladoInput(){
-        if(Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP))
+        if(Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP)){
             y+=Constantes.TILE_SIZE;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.S) || Gdx.input.isKeyJustPressed(Input.Keys.DOWN))
+            this.setCabeza(1, 3);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.S) || Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
             y-=Constantes.TILE_SIZE;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.A) || Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+            this.setCabeza(2, 0);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.A) || Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
             x-=Constantes.TILE_SIZE;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.D) || Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
+            this.setCabeza(1, 1);
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.D) || Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
             x+=Constantes.TILE_SIZE;
+            this.setCabeza(1, 2);
+        }
     }
-    public void dibujarPlayer(ShapeRenderer shape){
+    public void dibujarPlayer(ShapeRenderer shape){//metodo temporal-> dibujar un cuadrado en lugar de imagen
         shape.setColor(Color.BLUE);
         shape.rect(x, y, Constantes.TILE_SIZE, Constantes.TILE_SIZE);
     }
+    public void dibujarPlayer(SpriteBatch batch){
+        if(playerSprite!=null)
+        batch.draw(playerSprite, x, y,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+    }
     public void setCabeza(int col, int fila){
-        //Vacio por momento, falta crear el spriteSheet
+        playerSprite= new TextureRegion(sheetCabeza,col*32,fila*32,32,32);
     }
     public void setTorso(int col, int fila){
      //Vacio por momento, falta crear el spriteSheet
